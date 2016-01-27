9fd13d3ff56e592fce4eae9865820f538b92efe6
Jonatan Nyberg <jonatan.nyberg@infviz.com>
2016-01-27 13:20:01 +0100

    wip

---
+++ Examples/Views/app.ts
@@ -0,0 +1,39 @@
+/// <reference path="../../orange.d.ts" />
+/// <reference path="../typings/tsd.d.ts" />
+/// <reference path="../bower_components/rxjs/ts/rx.all.d.ts" />
+
+var startReq = { windowLoaded: false, templatesLoaded: false};
+
+function tryStartup() {
+
+    if (!startReq.windowLoaded || !startReq.templatesLoaded)
+    	return;
+
+    var application = new Application();
+    application.run();
+}
+
+window.onload = () => {
+    startReq.windowLoaded = true;
+    tryStartup();
+};
+
+TemplateLoader.onload = () => {
+    startReq.templatesLoaded = true;
+    tryStartup();
+};
+
+class Application {
+
+	public run(): void {
+		
+		var container = new Orange.Modularity.Container();
+
+		var controlManager = new Orange.Controls.ControlManager(container);
+        
+        container.registerInstance(Orange.Controls.ControlManager, controlManager);
+        controlManager.manage(document.body);
+	}
+}
+
+
--- Examples/Views/views.md
+++ Examples/Views/views.md
@@ -1,5 +1,10 @@
 
 ## Views
 
+The examples in the the *Views* example folder illustrates how views can be used in a nested fashion to achieve complex behaviors. 
+
+Take a look at the [example](index.html) and the code
+
+
 #### Where to go next?
 [Controls](../Controls/controls.md)
